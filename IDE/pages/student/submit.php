bff5d0389c851a6abd4659ee995b7279421eae2d
corneliusdavid97 <cornelius.david97@gmail.com>
2017-11-29 09:49:47 +0700

    aduhhh

--- IDE/pages/student/submit.php
+++ IDE/pages/student/submit.php
@@ -68,11 +68,11 @@
 							<div class="form-group row">
 						    <label  class="col-sm-2 col-form-label">Uploaded File</label>
 						    <div class="col-sm-10">
-                              <?php 
-                                echo "<a href='../..".$temp2['fileDir']."' download>";
-                                echo (isset($temp2['title'])?basename($temp2['fileDir']):"null");
-                                echo "</a>";
-                              ?>
+									<?php 
+										echo "<a href='../..".$temp2['fileDir']."' download>";
+										echo (isset($temp2['title'])?basename($temp2['fileDir']):"null");
+										echo "</a>";
+									?>
 						    </div>
 						</div>
 
